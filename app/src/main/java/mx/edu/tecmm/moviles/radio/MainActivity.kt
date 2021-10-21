5ad746aeca1a9267eee476a2cc1521d6a5036322
Lalaisse <rosa_icy14@hotmail.com>
2021-10-21 10:23:11 -0500

    Terminado

--- app/src/main/java/mx/edu/tecmm/moviles/radio/MainActivity.kt
+++ app/src/main/java/mx/edu/tecmm/moviles/radio/MainActivity.kt
@@ -15,6 +15,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var edNum2: EditText
     lateinit var rdSuma: RadioButton
     lateinit var rdResta: RadioButton //Cuando se utiliza onListenner se inicializa
+    lateinit var rdMultiplicacion: RadioButton
+    lateinit var rdDivision: RadioButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,21 +26,28 @@ class MainActivity : AppCompatActivity() {
         edNum2= findViewById(R.id.edNum2);
         rdResta= findViewById(R.id.rdResta);
         rdSuma= findViewById(R.id.rdSuma);
+        rdMultiplicacion= findViewById(R.id.rdMultiplicacion);
+        rdDivision= findViewById(R.id.rdDivision);
 
 
     }
 
     fun presiono (view: View){
         Log.e("Boton", "Se presionó el botón")
-        val num1= edNum1.text.toString().toInt();
-        val num2= edNum2.text.toString().toInt();
-        var res:Int =0;
+        val num1= edNum1.text.toString().toDouble();
+        val num2= edNum2.text.toString().toDouble();
+        var res: Double =0.0;
         if (rdSuma.isChecked){
             res= num1+num2;
         }else if (rdResta.isChecked){
             res= num1-num2;
+        }else if (rdMultiplicacion.isChecked){
+            res= num1*num2;
+        }else if (rdDivision.isChecked){
+            res= num1/num2;
         }
         txtResultado.text= "$res";
+
 
 
     }
